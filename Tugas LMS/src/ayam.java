74bbe9005b730d46b29c1e0c966a14165ae3f064
akbarrt <albarnv10@gmail.com>
2025-03-11 00:50:49 +0700

    LMS Task

---
+++ Tugas LMS/src/ayam.java
@@ -0,0 +1,18 @@
+
+public class ayam {
+	
+    void berkokok() {
+        System.out.println("Ayam telah berkokok cukurukuk");
+    }
+
+    void bertelur() {
+        System.out.println("Ayam sedang mengerami telur-telurnya");
+    }
+
+    public static void main(String[] args) {
+        ayam ayamm = new ayam();
+        ayamm.berkokok();
+        ayamm.bertelur(); 
+    }
+}
+
