bb0dc2b575dc6eb9f11ffe3fab5dbf840f854c76
akbarrt <albarnv10@gmail.com>
2025-03-20 09:15:37 +0700

    Task Modul 3

--- MODUL 3/main.java
+++ MODUL 3/main.java
@@ -1,6 +1,15 @@
-class karaktergame {
-	Private String nama;
-	Private int kesehatan;
-	
-	public karaktergame(String nama)
+public class Main {
+    public static void main(String[] args) {
+        KarakterGame karakterUmum = new KarakterGame("Karakter Umum", 100);
+        Pahlawan brimstone = new Pahlawan("Brimstone", 150);
+        Musuh viper = new Musuh("Viper", 200);
+
+        System.out.println("Status Awal Kesehatan:");
+        System.out.println(brimstone.getNama() + " Kesehatan: " + brimstone.getKesehatan());
+        System.out.println(viper.getNama() + " Kesehatan: " + viper.getKesehatan());
+
+        System.out.println("\nPertarungan Dimulai!");
+        brimstone.serang(viper);
+        viper.serang(brimstone);
+    }
 }
